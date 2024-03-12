5451076c775255db7bdd6d8ca837c8525412aeb3
GCET CSE 2022 <163174127+GCET-CSE2022@users.noreply.github.com>
2024-03-12 23:41:02 +0530

    adding files to GitHub

---
+++ HelloWorld.cpp
@@ -0,0 +1,22 @@
+
+// FOR TURBO C++
+
+#include <iostream>
+#include <conio.h>
+
+void main()
+{
+    clrscr();                       // Clear the screen
+    cout << "Hello World!" << endl; // Display the message
+    getch();                        // Wait for a key press
+}
+
+// FOR RECENT COMPILERS (C++ standard)
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!" << endl;
+    return 0;
+}
