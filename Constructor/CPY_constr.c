ff1e05ee59d17fcb8a1c7177d8a2019f0028d9e2
FAB <143941135+FaheemorFAB@users.noreply.github.com>
2024-04-28 20:24:24 +0530

    Update CPY_constr.c

--- Constructor/CPY_constr.c
+++ Constructor/CPY_constr.c
@@ -1,35 +1,55 @@
-#include<iostream.h>
-#include<conio.h>
-//using namespace std;
+/*```
+
+The provided C++ code demonstrates the use of constructors. Let's break it down:
+
+1. You've defined a class called `myclass`.
+2. The `myclass` has two private integer data members: `x` and `y`.
+3. It contains two constructors:
+   - The **default constructor** initializes `x` to 10 and `y` to 20.
+   - The **copy constructor** takes an object of `myclass` as an argument and copies its values to the newly created object.
+4. The `display()` method prints the values of `x` and `y`.
+
+When you run this program, it will create two objects (`obj1` and `obj2`) of the `myclass` type. The default constructor will be called for `obj1`, and the copy constructor will be called for `obj2`, copying the values from `obj1`.
+
+The output will show the values of `x` and `y` for both objects. In this case, it will display:
+
+```
+Default Constructor Called
+x = 10    y = 20
+Copy Constructor Called
+x = 10    y = 20
+```
+*/
+#include <iostream>
+#include <conio.h>
+
 class myclass
 {
-    private:
-	int x;
-	int y;
-    public:
-	myclass()
-	{
-	   cout<<"\n Default Constructor Called\n";
-	   x=10;
-	   y=20;
-	}
-	myclass(myclass &k)
-	{
-	   cout<<"\n Copy  Constructor Called\n";
-	   x=k.x;
-	   y=k.y;
-	}
+private:
+    int x;
+    int y;
 
+public:
+    myclass()
+    {
+        std::cout << "\n Default Constructor Called\n";
+        x = 10;
+        y = 20;
+    }
+    myclass(myclass &k)
+    {
+        std::cout << "\n Copy Constructor Called\n";
+        x = k.x;
+        y = k.y;
+    }
 
-	void display()
-	{
-	    cout<<"x =    " <<x<<"    y =  "<<y;
-
-	}
+    void display()
+    {
+        std::cout << "x = " << x << "    y = " << y;
+    }
 };
 
-
-void main()
+int main()
 {
     clrscr();
     myclass obj1;
