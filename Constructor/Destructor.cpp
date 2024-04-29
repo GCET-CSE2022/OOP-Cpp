adc0a2136069488aee36f756913f9fc0b832475b
FAB <143941135+FaheemorFAB@users.noreply.github.com>
2024-04-29 21:31:28 +0530

    Update Destructor.cpp

--- Constructor/Destructor.cpp
+++ Constructor/Destructor.cpp
@@ -1,3 +1,19 @@
+/*
+Destructor destroys objects that the Constructor has created within a C++ program.
+The name of the destructor is the same as the name of the class, except that it has a tilde (~) before its name.
+It is a good practice to declare the destructor after the Constructor has finished using it.
+
+When you run this program, you should see output similar to:
+Constructor Called
+Constructor Called
+The Size of Obj 2 is: 8
+Destructor Called
+Destructor Called
+
+This demonstrates how the order of constructor and destructor calls corresponds to the creation and destruction of objects in C++.
+The constructors are called when objects are created, and the destructors are called when objects go out of scope or are explicitly destroyed.
+*/
+
 #include <iostream>
 //#include <conio.h> // conio.h is not available in Visual Studio Code
 using namespace std;
